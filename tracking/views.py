159671bf376f936e448451c0bec44dccd38167e0
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:19:19 +0100

    implementation modelViewset Issues & Comments

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from tracking.models import Issue, Comment
+from tracking.serializers import IssueListSerializer, IssueDetailSerializer, CommentListSerializer, CommentDetailSerializer
+
+
+class IssuesViewset(ReadOnlyModelViewSet):
+    serializer_class = IssueListSerializer
+    detail_serializer_class = IssueDetailSerializer
+
+    def get_queryset(self):
+        projet_id = self.kwargs['project_pk']
+        return Issue.objects.filter(project_id=projet_id)
+
+
+class CommentsViewset(ReadOnlyModelViewSet):
+    serializer_class = CommentListSerializer
+    detail_serializer_class = CommentDetailSerializer
+
+    def get_queryset(self):
+        issue_id = self.kwargs['issue_pk']
+        return Comment.objects.filter(issue_id=issue_id)
+

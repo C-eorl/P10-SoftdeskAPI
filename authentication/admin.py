d890eb1cf257b5679c78dfbd6df17f4f4771a831
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:10:02 +0100

    ajout CustomUserAdmin - ajout de champs date de naissance, étre contacté, partage de donné

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from authentication.models import CustomUser
+
+
+@register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username','email', 'first_name', 'last_name', 'date_of_birth')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Informations supplémentaires", {
+            "fields": (
+                "date_of_birth",
+                "can_be_contacted",
+                "can_data_be_shared",
+            ),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Informations supplémentaires", {
+            "classes": ("wide",),
+            "fields": (
+                "date_of_birth",
+                "can_be_contacted",
+                "can_data_be_shared",
+            ),
+        }),
+    )

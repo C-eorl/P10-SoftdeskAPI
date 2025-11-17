4fce1d359b484a20a31e3c688705adde7d23a45d
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:10:45 +0100

    ajout Issues & Comments à Admin

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from projects.models import Project
+from tracking.models import Issue, Comment
+
+
+@register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    """Issue admin class"""
+    list_display = ('title', 'description', 'project', 'author', 'priority', 'tag', 'created_at', 'status')
+
+
+@register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """Comment admin class"""
+    list_display = ('description', 'issue', 'author', 'created_at')

0733bf53e8f46d2b1e06b31961844e72c10352de
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:18:43 +0100

    clean code

--- projects/views.py
+++ projects/views.py
@@ -1,32 +1,24 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from projects.models import Project, Contributor
-from projects.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorSerializer
+from projects.serializers import ProjectListSerializer, ProjectDetailSerializer, ContributorListSerializer, \
+    ContributorDetailSerializer
 
 
-class ProjectViewset(ReadOnlyModelViewSet):
+class ProjectViewset(ModelViewSet):
     serializer_class = ProjectListSerializer
     detail_serializer_class = ProjectDetailSerializer
     queryset = Project.objects.all()
 
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
-            return self.detail_serializer_class
-        return super().get_serializer_class()
-
-    def get_queryset(self):
-        queryset = Project.objects.all()
-
-        type_project = self.request.GET.get('type')
-        if type_project is not None:
-            queryset = queryset.filter(type=type_project)
-
-
-        return queryset  # Possibilité de filter .filter()
-
 
 class ContributorViewset(ReadOnlyModelViewSet):
-    serializer_class = ContributorSerializer
+    serializer_class = ContributorListSerializer
+    detail_serializer_class = ContributorDetailSerializer
+
 
     def get_queryset(self):
-        return Contributor.objects.all()
+        projet_id = self.kwargs['project_pk']
+        return Contributor.objects.filter(project_id=projet_id)

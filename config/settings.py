5d9ed418a3c898b3002db0ee11dc099be8130feb
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:17:06 +0100

    ajout DATETIME_FORMAT

--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 4,
+    'DATETIME_FORMAT': '%Y-%m-%d - %H:%M:%S',
 }

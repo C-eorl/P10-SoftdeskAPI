49954323b1e421568d138c39ea6ae2d9e0c08455
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:15:01 +0100

    implementation serializer contributorList

--- projects/serializers.py
+++ projects/serializers.py
@@ -3,23 +3,33 @@ from rest_framework.serializers import ModelSerializer
 from projects.models import Project, Contributor
 
 
-class ContributorSerializer(ModelSerializer):
+class ContributorDetailSerializer(ModelSerializer):
     class Meta:
         model = Contributor
         fields = '__all__'
 
 
+class ContributorListSerializer(ModelSerializer):
+    username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Contributor
+        fields = 'id', 'username'
+
+    def get_username(self, obj):
+        return obj.user.username
+
 class ProjectDetailSerializer(ModelSerializer):
     """ Serializer for project detail """
     author  =serializers.StringRelatedField(read_only=True)
-    contributors = ContributorSerializer(many=True, read_only=True)
+    contributors = ContributorListSerializer(many=True, read_only=True)
     issues_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = [
             'id', 'name', 'description', 'type',
-            'author', 'author_id', 'contributors', 'issues_count', 'created_at'
+            'author', 'contributors', 'issues_count', 'created_at'
         ]
 
     def get_issues_count(self, obj):

4641229c550e22ca4f71959cfd9c94345ebedd9b
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 15:10:27 +0100

    ajout Project & Contributor à Admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from projects.models import Project, Contributor
+
+
+@register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    """Project admin class"""
+    list_display = ('name', 'author', 'created_at', 'type')
+
+
+@register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    """Contributor admin class"""
+    list_display = ('user', 'project', 'created_at')

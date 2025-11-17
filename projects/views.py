90fc6032d5f1587a8b9441fb9d82fe216204f185
ceorl <dev.rocher.florian@gmail.com>
2025-11-17 17:34:00 +0100

    surcharge perform_create pour créer un contributor

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,3 @@
-from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from projects.models import Project, Contributor
@@ -13,6 +10,16 @@ class ProjectViewset(ModelViewSet):
     detail_serializer_class = ProjectDetailSerializer
     queryset = Project.objects.all()
 
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ProjectDetailSerializer
+        return ProjectListSerializer
+
+    def perform_create(self, serializer):
+        """Add project's author to contributor"""
+        project = serializer.save(author=self.request.user)
+        project.add_contributor(project.author)
+
 
 class ContributorViewset(ReadOnlyModelViewSet):
     serializer_class = ContributorListSerializer
@@ -20,5 +27,6 @@ class ContributorViewset(ReadOnlyModelViewSet):
 
 
     def get_queryset(self):
+        """Return contributors to project"""
         projet_id = self.kwargs['project_pk']
         return Contributor.objects.filter(project_id=projet_id)

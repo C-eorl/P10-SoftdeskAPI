a271ed59e27a5d51db2e289d3c13170cb5339aba
ceorl <dev.rocher.florian@gmail.com>
2025-11-19 11:42:17 +0100

    implementation method get_age => UserSerializer & serializers Signup

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from authentication.models import CustomUser
@@ -13,3 +13,35 @@ class UserSerializer(serializers.ModelSerializer):
             'id', 'username', 'email', 'first_name', 'last_name', 'date_of_birth',
             'age', 'can_be_contacted', 'can_data_be_shared', 'created_at'
         ]
+        read_only_fields = ['id', 'age', 'created_at']
+
+    def get_age(self, obj):
+        return obj.age
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'username', 'email', 'password', 'first_name', 'last_name', 'date_of_birth',
+            'can_be_contacted', 'can_data_be_shared',
+        ]
+
+        extra_kwargs = {
+            'username': {'required': True},
+            'email': {'required': True},
+            'date_of_birth': {'required': True},
+        }
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("L'adresse email est déjà utilisée")
+        return value
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user

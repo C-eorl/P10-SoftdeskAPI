710f342016895e11d31ebd07611f9d8fddfb103b
ceorl <dev.rocher.florian@gmail.com>
2025-11-25 09:42:08 +0100

    supression has_permission => IsContributor

--- tracking_projects/permissions.py
+++ tracking_projects/permissions.py
@@ -5,11 +5,7 @@ from tracking_projects.models import Project
 
 class IsContributor(permissions.BasePermission):
     """Permission to check if a user is project contributor """
-    message = "Vous devez être contributeur du projet pour accéder à l'ensemble di projet"
-    def has_permission(self, request, view):
-        project_id = view.kwargs.get('project_pk')
-        project = Project.objects.get(pk=project_id)
-        return project.contributors.filter(user=request.user).exists()
+    message = "Vous devez être contributeur du projet pour accéder à l'ensemble du projet"
 
     def has_object_permission(self, request, view, obj):
         """
@@ -30,9 +26,12 @@ class IsContributor(permissions.BasePermission):
 
 class IsAuthor(permissions.BasePermission):
     """Permission to check if a user is author """
+
     def has_object_permission(self, request, view, obj):
         if hasattr(obj, 'author'):
             author = obj.author
+        elif hasattr(obj, 'project'):
+            return obj.project.author == request.user
         else:
             return False
         return author == request.user
